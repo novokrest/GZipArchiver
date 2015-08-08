e7c1b6efbc106c0b034c13995a8782b42238032a
novokrest <novokrest013@gmail.com>
2015-08-08 18:44:34 +0400

    Implement Tester module

---
+++ Veeam.IntroductoryAssignment/Tester/GZipTester.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Tester
+{
+    abstract class ProgramTester
+    {
+        private readonly string _programPath;
+        private readonly int _expectedExitCode;
+
+        protected ProgramTester(string programPath, int expectedExitCode)
+        {
+            _programPath = programPath;
+            _expectedExitCode = expectedExitCode;
+        }
+
+        public string ProgramPath
+        {
+            get { return _programPath; }
+        }
+
+        public int ExpectedExitCode
+        {
+            get { return _expectedExitCode; }
+        }
+
+        protected void RunProgram(string arguments)
+        {
+            Console.WriteLine("Start {0} with arguments '{1}'", ProgramPath, arguments);
+            var startInfo = new ProcessStartInfo(ProgramPath, arguments)
+            {
+                UseShellExecute = false,
+                RedirectStandardOutput = true,
+                RedirectStandardError = true
+            };
+            var process = Process.Start(startInfo);
+            if (process == null)
+            {
+                throw new ProgramTesterException("Process is null");
+            }
+            //process.BeginOutputReadLine();
+            process.WaitForExit();
+            Console.Write("Process output: {0}", process.StandardOutput.ReadToEnd());
+            if (process.ExitCode != ExpectedExitCode)
+            {
+                throw new ProgramTesterException(String.Format("Unexpected exit code: expected={0}, actual={1}", ExpectedExitCode, process.ExitCode));
+            }
+        }
+
+        public abstract void Test();
+    }
+
+    abstract class GZipTester : ProgramTester
+    {
+        private readonly string _originalFileName;
+        private readonly string _convertedFileName;
+
+        public string Description { get; set; }
+
+        public string OriginalFileName
+        {
+            get { return _originalFileName; }
+        }
+
+        public string ConvertedFileName
+        {
+            get { return _convertedFileName; }
+        }
+
+        protected GZipTester(string programPath, string originalFileName, string convertedFileName, int compressExitCode)
+            : base(programPath, compressExitCode)
+        {
+            _originalFileName = originalFileName;
+            _convertedFileName = convertedFileName;
+        }
+
+        public override void Test()
+        {
+            Console.WriteLine(Description);
+            try
+            {
+                RunProgram(GetArguments());
+                RunProgram(GetArguments());
+                Console.WriteLine("Test SUCCESS");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Test FAILED: " + e.Message);
+                throw;
+            }
+            Console.WriteLine("Test completed");
+            Console.WriteLine();
+        }
+
+        protected abstract string GetArguments();
+    }
+
+    class GZipDecompressTester : GZipTester
+    {
+        public GZipDecompressTester(string programPath, string originalFileName, string convertedFileName, int compressExitCode)
+            : base(programPath, originalFileName, convertedFileName, compressExitCode)
+        {
+        }
+
+        protected override string GetArguments()
+        {
+            return String.Format("decompress {0} {1}", OriginalFileName, ConvertedFileName);
+        }
+    }
+
+    class GZipCompressTester : GZipTester
+    {
+        public GZipCompressTester(string programPath, string originalFileName, string convertedFileName, int compressExitCode)
+            : base(programPath, originalFileName, convertedFileName, compressExitCode)
+        {
+        }
+
+        protected override string GetArguments()
+        {
+            return String.Format("compress {0} {1}", OriginalFileName, ConvertedFileName);
+        }
+    }
+
+    internal class ProgramTesterException : Exception
+    {
+        public ProgramTesterException(string message)
+            : base(message)
+        {
+        }
+
+        public ProgramTesterException(string message, Exception innerException)
+            : base(message, innerException)
+        {
+        }
+    }
+}
---
+++ Veeam.IntroductoryAssignment/Tester/Program.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.IO;
+
+namespace Tester
+{
+    class Program
+    {
+        static void Main()
+        {
+            var solutionDirPath =
+                Path.GetDirectoryName(
+                    Path.GetDirectoryName(
+                        Path.GetDirectoryName(Environment.CurrentDirectory)));
+
+            var gzipBinSolutionRelativePath = @"Veeam.IntroductoryAssignment\bin\Debug\GZipTest.exe";
+            var programPath = solutionDirPath + Path.DirectorySeparatorChar + gzipBinSolutionRelativePath;
+
+            var smallFileName = "small.pdf";
+            var smallArchiveName = smallFileName + ".gz";
+            var smallUnpackedName = "unpacked_" + smallFileName;
+
+            var bigFileName = Path.GetFullPath("batman.pdf");
+            var bigArchiveName = bigFileName + ".gz";
+            var bigUnpackedName = "unpacked_" + bigFileName;
+
+            var nonExistentFileName = "nullfile";
+
+            try
+            {
+                new GZipCompressTester(programPath, smallFileName, smallArchiveName, 0)
+                {
+                    Description = "Test COMPRESS on small file. Archive doesn't exist."
+                }
+                    .Test();
+
+                new GZipDecompressTester(programPath, smallArchiveName, smallUnpackedName, 0)
+                {
+                    Description = "Test DECOMPRESS on small file. Unpacked doesn't exist."
+                }
+                    .Test();
+
+                new GZipCompressTester(programPath, smallFileName, smallArchiveName, 0)
+                {
+                    Description = "Test COMPRESS on small file. Archive already exists"
+                }
+                    .Test();
+
+                new GZipDecompressTester(programPath, smallArchiveName, smallUnpackedName, 0)
+                {
+                    Description = "Test DECOMPRESS on small file. Unpacked already exists"
+                }
+                    .Test();
+
+                new GZipCompressTester(programPath, nonExistentFileName, nonExistentFileName,
+                    1)
+                {
+                    Description = "Test COMPRESS on nonexistent file."
+                }
+                    .Test();
+
+                new GZipDecompressTester(programPath, nonExistentFileName, nonExistentFileName,
+                    1)
+                {
+                    Description = "Test DECOMPRESS on nonexistent file."
+                }
+                    .Test();
+
+                new GZipCompressTester(programPath, smallFileName, smallFileName, 1)
+                {
+                    Description = "Test COMPRESS on 'myself'"
+                }
+                    .Test();
+
+                new GZipDecompressTester(programPath, smallArchiveName, smallArchiveName, 1)
+                {
+                    Description = "Test DECOMPRESS on 'myself'"
+                }
+                    .Test();
+
+                new GZipDecompressTester(programPath, smallFileName, smallUnpackedName,
+                    1)
+                {
+                    Description = "Test DECOMPRESS on incorrect archive."
+                }
+                    //.Test()
+                    ;
+
+            new GZipCompressTester(programPath, bigFileName, bigArchiveName, 0)
+            {
+                Description = "Test COMPRESS on big file. Archive doesn't exist."
+            }
+            //.Test()
+            ;
+
+            new GZipDecompressTester(programPath, bigArchiveName, bigUnpackedName, 0)
+            {
+                Description = "Test DECOMPRESS on big file. Unpacked doesn't exist."
+            }
+            //.Test()
+            ;
+
+                Console.WriteLine("!!!SUCCESS!!!");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("!!!!!FAIL!!!!!");
+                return;
+            }
+
+            new object();
+        }
+    }
+}
